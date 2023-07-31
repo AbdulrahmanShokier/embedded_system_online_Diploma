adc69b6397afe33abd8c1f674f231d2f7449a670
Abdulrahman Shokier <you@example.com>
2023-07-31 14:42:43 +0300

    3rd Assignment

---
+++ Assignment 3/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 31, 2023
+ *      Author: abdo4
+ */
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter the problem no.");
+	fflush(stdin)fflush(stdout);
+	scanf("%d",x);
+	switch(x)
+	{
+	case 1:
+	{
+
+	}
+	}
+	return 0;
+}
