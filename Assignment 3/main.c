f815d0fc94657d7f99138a9dc57264b8774ab0e4
Abdulrahman Shokier <you@example.com>
2023-07-31 18:06:26 +0300

    3rd and 4th

--- Assignment 3/main.c
+++ Assignment 3/main.c
@@ -56,12 +56,78 @@ int main()
 		float arr[n];
 		for(i=0;i<n;i++)
 		{
-			printf("enter number:\n");
+			printf("%d. enter number:\n",i+1);
 			fflush(stdin); fflush(stdout);
 			scanf("%f",&arr[i]);
 			sum+=arr[i];
 		}
 		printf("the average=%.3f",avg=sum/(i));
+		break;
+	}
+	case 3:
+	{
+		int r,c;
+		int i,j;
+		printf("enter the rows and columns:\n");
+		fflush(stdin); fflush(stdout);
+		scanf("%d%d",&r,&c);
+		int arr[r][c];
+		printf("enter the elements of the matrix:\n");
+		for (i=0;i<r;i++)
+		{
+			for (j=0;j<c;j++)
+			{
+				printf("enter n%d%d",i+1,j+1);
+				fflush(stdin); fflush(stdout);
+				scanf("%d",&arr[i][j]);
+			}
+		}
+		printf("\nentered matrix\n");
+		for (i=0;i<r;i++)
+		{
+			for (j=0;j<c;j++)
+			{
+				printf("%d",arr[i][j]);
+			}
+			printf("\n");
+		}
+		printf("transposed matrix\n");
+		for (i=0;i<r;i++)
+		{
+			for (j=0;j<c;j++)
+			{
+				printf("%d",arr[j][i]);
+			}
+			printf("\n");
+		}
+		break;
+	}
+	case 4:
+	{
+		int n,x,l,i;
+		printf("enter number of elements:\n");
+		fflush(stdin); fflush(stdout);
+		scanf("%d",&n);
+		int arr[n];
+		for(i=0;i<n;i++)
+		{
+			printf("%d. enter number:\n",i+1);
+			fflush(stdin); fflush(stdout);
+			scanf("%d",&arr[i]);
+		}
+		printf("enter element to be inserted:\n");
+		fflush(stdin); fflush(stdout);
+		scanf("%d",&x);
+		printf("enter its location:\n");
+		fflush(stdin); fflush(stdout);
+		scanf("%d",&l);
+		arr[l-1]=x;
+		printf("array= ");
+		for(i=0;i<n;i++)
+		{
+			printf("%d  ",arr[i]);
+		}
+		break;
 	}
 	}
 	return 0;
